3e7c92447d47791e844d4b5cef176d6c56f90905
Anders Gerner <github@andersgerner.dk>
2023-08-13 17:19:07 +0200

    Add tier template

--- apps/next/_next.config.js
+++ apps/next/_next.config.js
@@ -1,19 +1,12 @@
 const { withExpo } = require('@expo/next-adapter')
-require('./src/env.mjs')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // reanimated (and thus, Moti) doesn't work with strict mode currently...
+  // https://github.com/nandorojo/moti/issues/224
+  // https://github.com/necolas/react-native-web/pull/2330
+  // https://github.com/nandorojo/moti/issues/224
+  // once that gets fixed, set this back to true
   reactStrictMode: false,
   transpilePackages: [
     'react-native',
